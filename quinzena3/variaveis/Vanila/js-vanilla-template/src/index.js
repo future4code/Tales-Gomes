68af4e689c49f0f6e6a86a108e7ccd41ee855db0
Tales Gomes <tales.gomes@hotmail.com>
2021-03-30 20:24:00 -0300

    terminado-exercicio-aula10

---
+++ quinzena3/variaveis/Vanila/js-vanilla-template/src/index.js
@@ -0,0 +1,92 @@
+// Exercício 1
+/* a = 10
+b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b) */
+// Neste exercício A e B tem 10 na parte superior do código, console B vai puxar o valor de B que é 10, mas embaixo foi definido que B iria valer 5, então iria ficar 10,5 
+/* 
+const newLocal = [Lasanha, Panqueca]; */
+// Exercício 2
+/* a = 10
+b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c) */
+// A ideia dessa exercício é poder fazer com o 10 vá para o A, o resultado do A vai até o C, o resultado do C vai ir para o B e o resultado do B vai para a letra A.
+
+/* 
+const primerioNome = 
+let idade =
+
+typeof primerioNome
+typeof idade
+
+console.log(primerioNome, idade) */
+
+// Quando usei esses comandos, apareceu "Unexpected identifier", acredito que foi porque não teve valor dentro da variável criada, então não teve como identificar qual é o valor de cada uma.
+ 
+/* const nome = "Tales"
+const idade= 24 */
+
+// Agora deu um erro na tela, acho que foi porque agora eu naõ estava tentando puxar o conteúdo das variáveis, elas são que uma informação jogada, sem definição
+
+/*  const primerioNome = "Tales"
+const idade = 24
+
+console.log("Olá", primerioNome,"Você tem", idade, "anos", "Onde 'nome' e 'idade' são os valores que o usuário inseriu") */
+
+//Agora eu coloquei todos os comandos e atribui aspas duplas e aspas simples para diferenciar as palavras no meu código
+
+//Exercício 2 - Criar perguntas
+
+/* let nomeEndereco = prompt("1. Onde você mora?")
+let nomePessoa = prompt("2. Qual é o seu nome?")
+let nomeCorFavorita = prompt("3. Qual é a sua cor favorita?")
+let nomeLugarMoradia = prompt("4. Onde você gostaria de morar daqui 5 anos?")
+let nomeComidaFavorita = prompt("5. Qual é a sua comida favorita?")
+
+console.log("Resposta:", nomeEndereco)
+console.log("Resposta:", nomePessoa)
+console.log("Reposta:", nomeCorFavorita)
+console.log("Reposta:", nomeLugarMoradia)
+console.log("Reposta:". nomeComidaFavorita) */
+
+//Aqui eu criei 5 gavetinhas diferentes para cada pergunta
+
+//Exercício 3 - 5 comidas no Array
+
+/* let arrayComidasFavoritas = ["Lasanha", "Panqueca", "Pastel", "Pizza", "Xis"]
+
+console.log(arrayComidasFavoritas["lasanha"])
+console.log(arrayComidasFavoritas["Panqueca"]) = comidaPergunta
+console.log(arrayComidasFavoritas["Pastel"])
+console.log(arrayComidasFavoritas["Pizza"])
+console.log(arrayComidasFavoritas["Xis"]) 
+
+console.log("Essas são as minhas comidas favoritas:",arrayComidasFavoritas)
+
+const comidaPergunta = prompt("Qual é a sua comida favorita")
+
+console.log(comidaPergunta, arrayComidasFavoritas) */
+
+//Exercício 4 - True 
+/* 
+const roupaAzul = prompt("1. Você está usando uma roupa azul?")
+
+const booleanoVerdadeiro = true
+console.log(roupaAzul, booleanoVerdadeiro)
+
+const roupaVermelha = prompt("2. Você está usando uma roupa vermelha?")
+
+const booelanoFalso = false
+console.log(roupaVermelha, booelanoFalso)
+
+let roupaPreta = prompt("3. Você está usando uma roupa preta?") 
+
+let booleanoFlexivel = true
+console.log(roupaPreta, booleanoFlexivel) */
